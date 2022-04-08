2c346eb2464720363a0418f5b87760bbb0715cc7
reyesmartinez <miguelreyesmr@correo.ugr.es>
2022-04-09 01:58:52 +0200

    Añadido funciones a miembros
    Se ha añadido la posibilidad de editar el perfil y de cambiar la contraseña

--- miembros/urls.py
+++ miembros/urls.py
@@ -1,9 +1,13 @@
+from django import views
 from django.urls import path
-#from .views import
+from .views import UserEditView, CambiarPasswordView
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     
-    #path('login/', BLA.as_View(), name='pagina-login'),
-    # Este path es necesario para Django
+    path('edit_perfil/', UserEditView.as_view(), name="editar-perfil"),
+    path('password/', CambiarPasswordView.as_view()),
+    path('password_success', views.password_success, name="password_success"),
     
 ]
--- miembros/views.py
+++ miembros/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
+from .forms import EditarPerfilForm, CambiarPasswordForm
 
-#class U
+class CambiarPasswordView(PasswordChangeView):
+    form_class = CambiarPasswordForm
+    template_name = 'registration/change_password.html'
+    success_url = reverse_lazy('password_success')
+
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
+
+
+class UserEditView(generic.UpdateView):
+    form_class = EditarPerfilForm
+    template_name = 'registration/edit_perfil.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
