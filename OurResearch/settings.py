ba4b037eb203b4445941b4fa7116dc2013b894c0
robermorji <robermorji@ugr.es>
2022-09-26 09:33:09 +0200

    Errores de configuracion
    Cambios en la configuración

--- OurResearch/settings.py
+++ OurResearch/settings.py
@@ -47,10 +47,10 @@ INSTALLED_APPS = [
     'django_social_share',
     'taggit',
     'noticias',
-    'miembros',
     'eventos',
+    'miembros',
     'publicaciones',
-    'proyectos',
+    'proyectos'
 ]
 
 TAGGIT_CASE_INSENSITIVE = True
@@ -135,26 +135,27 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT= os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
-# Aquí le decimos a Django donde va a estar la carpeta static
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'), 
+   os.path.join(BASE_DIR, "noticias\static"),
+   os.path.join(BASE_DIR, "miembros\static"),
+   os.path.join(BASE_DIR, "eventos\static"),
+   os.path.join(BASE_DIR, "proyectos\static"),
+   os.path.join(BASE_DIR, "publicaciones\static"),
 ]
 
+# Aquí le decimos a Django donde va a estar la carpeta static
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
